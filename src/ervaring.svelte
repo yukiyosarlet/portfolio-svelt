ab4cf2b6f33c37b4f093acb51a04a51aef225202
yukiyo.sarlet <yu.sarlet@gmail.com>
2025-06-23 14:34:41 +0200

    create-klaar

---
+++ src/ervaring.svelte
@@ -0,0 +1,164 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div class="header">
+            <h1>Mijn werk ervaring</h1>
+        </div>
+
+        <div class="main-container">
+            <div class="ervaring">
+                <div class="job">
+                    <div class="picture">
+                        <img src="img/" class="jobImg" alt="job">
+                    </div>
+                    <div class="info-job">
+                        <div class="werk">
+                            <p>Vakantiejob Wienerberger</p>
+                            <p>Wienerberger, Zonnebeke</p>
+                        </div>
+                        <div class="tijd">
+                            Jul 2016 – Aug 2016
+                        </div>
+                    </div>
+                    <div class="werk">
+                        <p>Monitor speelpleinwerking</p>
+                        <p>gemeente Kluisbergen</p>
+                    </div>
+                </div>
+
+                <div class="job">
+                    <div class="picture">
+                        <img src="img/" class="jobImg" alt="job">
+                    </div>
+
+                    <div class="info-job">
+                        <div class="werk">
+                            <p>Vakantiejob Wienerberger</p>
+                            <p>Wienerberger, Zonnebeke</p>
+                        </div>
+                        <div class="tijd">
+                            jul 2017- aug 2017
+                        </div>
+                    </div>
+                    <div class="werk">
+                        <p>Logistiek</p>
+                        <p>Woonzorgcentrum Marialove, Heestert</p>
+                    </div>
+                </div>
+
+                <div class="job">
+                    <div class="picture">
+                        <img src="img/" class="jobImg" alt="job">
+                    </div>
+                    <div class="info-job">
+                        <div class="werk">
+                            <p>Vakantiejob Wienerberger</p>
+                            <p>Wienerberger, Zonnebeke</p>
+                        </div>
+                        <div class="tijd">
+                            Jul 2018- aug 2021
+                        </div>
+                    </div>
+                    <div class="werk">
+
+                        <p>Ik moest helpen met het maken van SOP's en het ordenen van de oude SOP's en </p>
+                    </div>
+                </div>
+
+                <div class="job">
+                    <div class="picture">
+                        <img src="img/" class="jobImg" alt="job">
+                    </div>
+                    <div class="info-job">
+                        <div class="werk">
+                            <p>Verpleging</p>
+                            <p>Woonzorgcentrum Westevier, Brugge</p>
+                        </div>
+                        <div class="tijd">
+                            Mei 2021- jun 2021
+                        </div>
+                    </div>
+                    <div class="werk">
+
+                        <p>student (stage opleiding Graduaat verpleegkundige)</p>
+                    </div>
+                </div>
+
+                <div class="job">
+                    <div class="picture">
+                        <img src="img/" class="jobImg" alt="job">
+                    </div>
+                    <div class="info-job">
+                        <div class="werk">
+                            <p>Verpleging</p>
+                            <p>Az Damiaan, Oostende</p>
+                        </div>
+                        <div class="tijd">
+                            Sep 2021 – okt 2021
+                        </div>
+                    </div>
+                    <div class="werk">
+                        <p>student (stage opleiding Graduaat verpleegkundige)</p>
+                    </div>
+                </div>
+
+                <div class="job">
+                    <div class="picture">
+                        <img src="img/" class="jobImg" alt="job">
+                    </div>
+                    <div class="info-job">
+                        <div class="werk">
+                            <p>DLC System integration</p>
+                            <p>Programmeur</p>
+                        </div>
+                        <div class="tijd">
+                            1/juli - 19/ juli 2024
+                        </div>
+                    </div>
+                    <div class="uitleg">
+                        <p>1.5week als instalateur en 1.5 als programmeur</p>
+                        <p>heb SvelteKit,figma, markdown, cloudfare</p>
+                    </div>
+                </div>
+
+                <div class="job">
+                    <div class="picture">
+                        <img src="img/" class="jobImg" alt="job">
+                    </div>
+                    <div class="info-job">
+                        <div class="werk">
+                            <p>Portfolio</p>
+                            <p>Persoonlijk project</p>
+                        </div>
+                        <div class="tijd">
+                            26/aug - ...
+                        </div>
+                    </div>
+                    <div class="werk">
+
+                        <p>gemaakt met scss/css, html</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</main>
+
+<style lang="scss">
+
+    .welcome {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      color: #0F0;
+      font-size: 48px; 
+      font-weight: bold;
+      z-index: 1;
+      text-shadow: 0 0 10px #0F0, 0 0 20px #0F0;
+    }
+ 
+</style>
---
+++ src/info.svelte
@@ -0,0 +1,176 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+ <div class="information">
+            <div class="personal">
+                <div class="titel">
+                    <h2>Informatie</h2>
+                </div>
+                <div class="question">
+                    Naam:
+                </div>
+                <div class="awnser">
+                    Yukiyo
+                </div>
+                <div class="question">
+                    Famillienaam:
+                </div>
+                <div class="awnser">
+                    Sarlet
+                </div>
+                <div class="question">
+                    Adres:
+                </div>
+                <div class="awnser">
+                    <p>Beukendreef 7,</p>
+                    <p>9690 kluisbergen</p>
+                </div>
+                <div class="question">
+                    Opleiding:
+                </div>
+                <div class="awnser">
+                    <p>Front and backend</p>
+                    <p>Webdevelopment</p>
+                </div>
+            </div>
+            <div class="socials">
+                <div class="titel">
+                    <h2>Gegevens</h2>
+                </div>
+                <div class="question">
+                    Number:
+                </div>
+                <div class="awnser">
+                    +32 470 86 91 76
+                </div>
+                <div class="question">
+                    Email:
+                </div>
+                <div class="awnser">
+                    yu.sarlet@gmail.com
+                </div>
+                <div class="question">
+                    LinkdIn:
+                </div>
+                <div class="awnser">
+                    <a href="https://www.linkedin.com/in/yukiyo-sarlet-483477311/">
+                        LinkdIn Yukiyo Sarlet</a>
+                </div>
+                <div class="question">
+                    Github:
+                </div>
+                <div class="awnser">
+                    <a href="  https://github.com/yukiyosarlet">
+                        Github Yukiyo Sarlet</a>
+
+                </div>
+            </div>
+            <div class="language">
+                <div class="titel">
+                    <h2>Talen</h2>
+                </div>
+                <div class="taal">
+                    Nederlands (moedertaal)
+                </div>
+                <div class="taal">
+                    Engels (vloeiend)
+                </div>
+                <div class="taal">
+                    Frans (redelijk)
+                </div>
+            </div>
+            <div class="hobbies">
+                <div class="titel">
+                    <h2>Hobbies</h2>
+                </div>
+                <div class="hobby">
+                    fitness
+                </div>
+                <div class="hobby">
+                    gamen
+                </div>
+                <div class="hobby">
+                    zwemmen
+                </div>
+                <div class="hobby">
+                    zeilen
+                </div>
+            </div>
+            <div class="skills">
+                <div class="titel">
+                    <h2>Skills</h2>
+                </div>
+                <div class="skill">
+                    HTML
+                </div>
+                <div class="skill">
+                    Javascript
+                </div>
+                <div class="skill">
+                    Scss/css
+                </div>
+                <div class="skill">
+                    PHP
+                </div>
+                <div class="skill">
+                    API
+                </div>
+                <div class="skill">
+                    SQL
+                </div>
+                <div class="skill">
+                    Svelte
+                </div>
+                <div class="skill">
+                    Photoshop
+                </div>
+                <div class="skill">
+                    Illustrator
+                </div>
+            </div>
+
+            <div class="opleiding">
+                <div class="titel">
+                    <h2>Opleiding</h2>
+                </div>
+                <div class="datum">
+                    sep 2016 - jun 2019
+                </div>
+                <div class="omschrijving">
+                    <p>Atheneum Avelgem</p>
+                    <p> Sociaal technische wetenschappen</p>
+                </div>
+                <div class="datum">
+                    Feb 2022- heden
+                </div>
+                <div class="omschrijving">
+                    <p> Howest; Kortrijk</p>
+                    <p>front and backend web development</p>
+                </div>
+            </div>
+
+            <div class="ervaring">
+                <div class="titel">
+                    <h2>ervaring</h2>
+                </div>
+                <div class="datum">
+                    1/juli - 19/ juli 2024
+                </div>
+                <div class="omschrijving">
+                    DLC System integration
+                    <p>1.5 week als instalateur</p>
+                    <p>1.5 week als programmeur</p>
+                </div>
+            </div>
+        </div> 
+</main>
+
+<style lang="scss">
+
+ 
+ 
+</style>
